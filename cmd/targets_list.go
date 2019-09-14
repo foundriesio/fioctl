bf8a25bfdd330bd3527cf06191112bd4aa7c92df
Andy Doan <andy@foundries.io>
2019-09-14 11:45:11 -0500

    targets: list by hwid and tag support
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/targets_list.go
+++ cmd/targets_list.go
@@ -17,16 +17,22 @@ var targetListCmd = &cobra.Command{
 	Run:   doTargetsList,
 }
 
-var listRaw bool
+var (
+	listRaw    bool
+	listByTag  string
+	listByHwId string
+)
 
 func init() {
 	targetsCmd.AddCommand(targetListCmd)
 	targetListCmd.Flags().BoolVarP(&listRaw, "raw", "r", false, "Print raw targets.json")
+	targetListCmd.Flags().StringVarP(&listByHwId, "by-hwid", "", "", "Only list targets that match the given hardware-id")
+	targetListCmd.Flags().StringVarP(&listByTag, "by-tag", "", "", "Only list targets that match the given tag")
 }
 
 func doTargetsList(cmd *cobra.Command, args []string) {
 	factory := viper.GetString("factory")
-	logrus.Debugf("Listing targets for %s", factory)
+	logrus.Debugf("Listing targets for %s hwid(%s) tag(%s)", factory, listByHwId, listByTag)
 
 	if listRaw {
 		body, err := api.TargetsListRaw(factory)
@@ -44,20 +50,28 @@ func doTargetsList(cmd *cobra.Command, args []string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	for name := range targets.Signed.Targets {
-		fmt.Println("=", name)
-		target := targets.Signed.Targets[name]
-		hash := hex.EncodeToString(target.Hashes["sha256"])
-		fmt.Printf("\tsha256:%s\n", hash)
+	for name, target := range targets.Signed.Targets {
 		custom, err := api.TargetCustom(target)
 		if err != nil {
 			fmt.Printf("ERROR: %s\n", err)
-		} else {
-			fmt.Printf("\tversion:%s\n", custom.Version)
-			fmt.Printf("\tformat:%s\n", custom.TargetFormat)
-			if len(custom.Tags) > 0 {
-				fmt.Printf("\ttags:%s\n", strings.Join(custom.Tags, ","))
-			}
+			continue
+		}
+		if len(listByHwId) > 0 && !intersectionInSlices([]string{listByHwId}, custom.HardwareIds) {
+			logrus.Debugf("Target(%r) does not match hwid)", target)
+			continue
+		}
+		if len(listByTag) > 0 && !intersectionInSlices([]string{listByTag}, custom.Tags) {
+			logrus.Debugf("Target(%r) does not include tag)", target)
+			continue
+		}
+		fmt.Println("=", name)
+		hash := hex.EncodeToString(target.Hashes["sha256"])
+		fmt.Printf("\tsha256:%s\n", hash)
+		fmt.Printf("\tversion:%s\n", custom.Version)
+		fmt.Printf("\thwids:%s\n", strings.Join(custom.HardwareIds, ","))
+		fmt.Printf("\tformat:%s\n", custom.TargetFormat)
+		if len(custom.Tags) > 0 {
+			fmt.Printf("\ttags:%s\n", strings.Join(custom.Tags, ","))
 		}
 	}
 }
