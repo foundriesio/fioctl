48e0767426a5ec51b682d66aafc8e49247caa290
Andy Doan <andy@foundries.io>
2019-10-18 14:42:23 -0500

    Fix formatting in `device show`
    The extra \t's were just copy/pasted from the device-list command.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/device_show.go
+++ cmd/device_show.go
@@ -29,31 +29,31 @@ func doDeviceShow(cmd *cobra.Command, args []string) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("\tUUID:\t\t%s\n", device.Uuid)
-	fmt.Printf("\tOwner:\t\t%s\n", device.Owner)
-	fmt.Printf("\tFactory:\t%s\n", device.Factory)
-	fmt.Printf("\tTarget:\t\t%s / sha256(%s)\n", device.TargetName, device.OstreeHash)
-	fmt.Printf("\tOstree Hash:\t%s\n", device.OstreeHash)
-	fmt.Printf("\tCreated:\t%s\n", device.CreatedAt)
-	fmt.Printf("\tLast Seen:\t%s\n", device.LastSeen)
+	fmt.Printf("UUID:\t\t%s\n", device.Uuid)
+	fmt.Printf("Owner:\t\t%s\n", device.Owner)
+	fmt.Printf("Factory:\t%s\n", device.Factory)
+	fmt.Printf("Target:\t\t%s / sha256(%s)\n", device.TargetName, device.OstreeHash)
+	fmt.Printf("Ostree Hash:\t%s\n", device.OstreeHash)
+	fmt.Printf("Created:\t%s\n", device.CreatedAt)
+	fmt.Printf("Last Seen:\t%s\n", device.LastSeen)
 	if len(device.Tags) > 0 {
-		fmt.Printf("\tTags:\t\t%s\n", strings.Join(device.Tags, ","))
+		fmt.Printf("Tags:\t\t%s\n", strings.Join(device.Tags, ","))
 	}
 	if len(device.DockerApps) > 0 {
-		fmt.Printf("\tDocker Apps:\t%s\n", strings.Join(device.DockerApps, ","))
+		fmt.Printf("Docker Apps:\t%s\n", strings.Join(device.DockerApps, ","))
 	}
 	if len(device.Status) > 0 {
-		fmt.Printf("\tStatus:\t\t%s\n", device.Status)
+		fmt.Printf("Status:\t\t%s\n", device.Status)
 	}
 	if len(device.CurrentUpdate) > 0 {
-		fmt.Printf("\tUpdate Id:\t%s\n", device.CurrentUpdate)
+		fmt.Printf("Update Id:\t%s\n", device.CurrentUpdate)
 	}
 	if device.Hardware != nil {
-		b, err := json.MarshalIndent(device.Hardware, "\t\t", "  ")
+		b, err := json.MarshalIndent(device.Hardware, "\t", "  ")
 		if err != nil {
 			fmt.Println("Unable to marshall hardware info: ", err)
 		}
-		fmt.Printf("\tHardware Info:\n\t\t")
+		fmt.Printf("Hardware Info:\n\t")
 		os.Stdout.Write(b)
 		fmt.Println("")
 	}
