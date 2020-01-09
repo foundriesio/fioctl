18dcf71bee2ab81c3e0f0f9658758b5497897630
Andy Doan <andy@foundries.io>
2020-01-09 09:02:21 -0600

    Move token flag to subcommands that use it
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/device.go
+++ cmd/device.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var deviceCmd = &cobra.Command{
@@ -12,4 +16,9 @@ var deviceCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(deviceCmd)
+	deviceCmd.PersistentFlags().StringP("token", "t", "", "API token from https://app.foundries.io/settings/tokens/")
+	if err := viper.BindPFlags(deviceCmd.PersistentFlags()); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
