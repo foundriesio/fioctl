ea8d8ee4eee146feabaf146f07fec06cf6846b48
Marc Streeter <marc@foundries.io>
2020-02-18 09:10:10 -0600

    Fixes: https://github.com/foundriesio/fioctl/issues/18
    added show detail
    
    Fixes: https://github.com/foundriesio/fioctl/issues/18

--- cmd/device_show.go
+++ cmd/device_show.go
@@ -32,6 +32,7 @@ func doDeviceShow(cmd *cobra.Command, args []string) {
 	fmt.Printf("UUID:\t\t%s\n", device.Uuid)
 	fmt.Printf("Owner:\t\t%s\n", device.Owner)
 	fmt.Printf("Factory:\t%s\n", device.Factory)
+	fmt.Printf("Up to date:\t%v\n", device.UpToDate)
 	fmt.Printf("Target:\t\t%s / sha256(%s)\n", device.TargetName, device.OstreeHash)
 	fmt.Printf("Ostree Hash:\t%s\n", device.OstreeHash)
 	fmt.Printf("Created:\t%s\n", device.CreatedAt)
