a29aae606aa05e8df3df385c76f6f5dcf0ed26d0
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2020-12-10 23:14:01 +0200

    FFTK-170 - ability to delete device group config

--- subcommands/config/delete.go
+++ subcommands/config/delete.go
@@ -9,17 +9,26 @@ import (
 )
 
 func init() {
-	cmd.AddCommand(&cobra.Command{
+	deleteCmd := &cobra.Command{
 		Use:   "delete <file>",
 		Short: "Delete file from the current configuration",
 		Run:   doConfigDelete,
 		Args:  cobra.ExactArgs(1),
-	})
+	}
+	cmd.AddCommand(deleteCmd)
+	deleteCmd.Flags().StringP("group", "g", "", "Device group to use")
 }
 
 func doConfigDelete(cmd *cobra.Command, args []string) {
 	factory := viper.GetString("factory")
-	logrus.Debugf("Deleting file from config for %s", factory)
+	group, _ := cmd.Flags().GetString("group")
+	filename := args[0]
 
-	subcommands.DieNotNil(api.FactoryDeleteConfig(factory, args[0]))
+	if group == "" {
+		logrus.Debugf("Deleting file %s from config for %s", filename, factory)
+		subcommands.DieNotNil(api.FactoryDeleteConfig(factory, filename))
+	} else {
+		logrus.Debugf("Deleting file %s from config for %s group %s", filename, factory, group)
+		subcommands.DieNotNil(api.GroupDeleteConfig(factory, group, filename))
+	}
 }
