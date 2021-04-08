dbe42cccb14913eb1da603d132ea9b9f40333bda
Andy Doan <andy@foundries.io>
2021-04-07 21:59:39 -0500

    Include device-group information into a factory status
    The status API now includes important information about device groups
    that helps operators understand/plan OTAs.
    
    Signed-off-by: Andy Doan <andy@foundries.io>, Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- client/foundries.go
+++ client/foundries.go
@@ -173,14 +173,24 @@ type TargetStatus struct {
 	IsOrphan     bool `json:"is-orphan"`
 }
 
+type DeviceGroupStatus struct {
+	Name            string `json:"name"`
+	DevicesTotal    int    `json:"devices-total"`
+	DevicesOnline   int    `json:"devices-online"`
+	DevicesOnLatest int    `json:"devices-on-latest"`
+	DevicesOnOrphan int    `json:"devices-on-orphan"`
+	Reinstalling    int    `json:"(re-)installing"`
+}
+
 type TagStatus struct {
-	Name            string         `json:"name"`
-	DevicesTotal    int            `json:"devices-total"`
-	DevicesOnline   int            `json:"devices-online"`
-	DevicesOnLatest int            `json:"devices-on-latest"`
-	DevicesOnOrphan int            `json:"devices-on-orphan"`
-	LatestTarget    int            `json:"latest-target"`
-	Targets         []TargetStatus `json:"targets"`
+	Name            string              `json:"name"`
+	DevicesTotal    int                 `json:"devices-total"`
+	DevicesOnline   int                 `json:"devices-online"`
+	DevicesOnLatest int                 `json:"devices-on-latest"`
+	DevicesOnOrphan int                 `json:"devices-on-orphan"`
+	LatestTarget    int                 `json:"latest-target"`
+	Targets         []TargetStatus      `json:"targets"`
+	DeviceGroups    []DeviceGroupStatus `json:"device-groups"`
 }
 
 type FactoryStatus struct {
