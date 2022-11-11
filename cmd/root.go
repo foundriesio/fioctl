897c789acd52b45cb8026f90e2aae576a9a2b65d
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2022-11-11 22:59:40 +0200

    Chore: upgrade cobra to 1.6.1
    I need some neat argument validation features which appeared in 1.5.0.
    So use this chance to upgrade corba all the way to latest.
    Also, replaced some deprecated function call.
    And, as a side effect a vagon of irrelevant dependencies are gone.
    
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- cmd/root.go
+++ cmd/root.go
@@ -163,7 +163,7 @@ $ fioctl completion fish > ~/.config/fish/completions/fioctl.fish
 `,
 	DisableFlagsInUseLine: true,
 	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-	Args:                  cobra.ExactValidArgs(1),
+	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		switch args[0] {
 		case "bash":
