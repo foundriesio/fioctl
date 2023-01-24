d786c303b2a2d85a71d0996dc196f0d97f8ccd44
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2023-01-24 15:19:32 +0200

    Fixup: correct order for actions in tuf rotate-all-keys for expired root
    In case of expired root role the first action to take must be the TUF root key rotation.
    Otherwise, the API will complain about the too low expires field value.
    
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- subcommands/keys/tuf_rotate_all_keys.go
+++ subcommands/keys/tuf_rotate_all_keys.go
@@ -69,7 +69,7 @@ func doRotateAllKeys(cmd *cobra.Command, unusedArgs []string) {
 	tufUpdatesCmd.SetArgs(args)
 	subcommands.DieNotNil(tufUpdatesCmd.Execute())
 
-	args = []string{"rotate-online-key", "-r", "targets,snapshot,timestamp", "-y", keyType}
+	args = []string{"rotate-offline-key", "-r", "root", "-k", credsFile, "-y", keyType}
 	tufUpdatesCmd.SetArgs(args)
 	subcommands.DieNotNil(tufUpdatesCmd.Execute())
 
@@ -77,7 +77,11 @@ func doRotateAllKeys(cmd *cobra.Command, unusedArgs []string) {
 	tufUpdatesCmd.SetArgs(args)
 	subcommands.DieNotNil(tufUpdatesCmd.Execute())
 
-	args = []string{"rotate-offline-key", "-r", "root", "-sk", credsFile, "-y", keyType}
+	args = []string{"rotate-online-key", "-r", "targets,snapshot,timestamp", "-y", keyType}
+	tufUpdatesCmd.SetArgs(args)
+	subcommands.DieNotNil(tufUpdatesCmd.Execute())
+
+	args = []string{"sign", "-k", credsFile}
 	tufUpdatesCmd.SetArgs(args)
 	subcommands.DieNotNil(tufUpdatesCmd.Execute())
 
