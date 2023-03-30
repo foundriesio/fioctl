34ed811c37c9a6668598bd29fbd22dd5a7cc8ed9
Mike Sul <mike.sul@foundries.io>
2023-03-30 12:53:44 +0200

    users: Print details about user teams
    Print scopes and device groups of each user team.
    
    Signed-off-by: Mike <mike.sul@foundries.io>

--- subcommands/users/cmd.go
+++ subcommands/users/cmd.go
@@ -2,12 +2,12 @@ package users
 
 import (
 	"github.com/cheynewallace/tabby"
+	"github.com/foundriesio/fioctl/client"
+	"github.com/foundriesio/fioctl/subcommands"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-
-	"github.com/foundriesio/fioctl/client"
-	"github.com/foundriesio/fioctl/subcommands"
+	"strings"
 )
 
 func NewCommand() *cobra.Command {
@@ -54,12 +54,23 @@ func doGetUser(api *client.Api, factory, user_id string) {
 	t.AddLine()
 	t.AddHeader("TEAMS")
 	for _, team := range user.Teams {
-		t.AddLine(team)
+		t.AddLine(team.Name)
+		var scopes []string
+		for _, s := range team.Scopes {
+			scopes = append(scopes, s[strings.Index(s, ":")+1:])
+		}
+		if len(scopes) > 0 {
+			t.AddLine("\tScopes: " + strings.Join(scopes, ", "))
+		}
+		if len(team.Groups) > 0 {
+			t.AddLine("\tGroups: " + strings.Join(team.Groups, ", "))
+		}
+		t.AddLine()
 	}
 	t.AddLine()
 	t.AddHeader("EFFECTIVE SCOPES")
 	for _, scope := range user.EffectiveScopes {
-		t.AddLine(scope)
+		t.AddLine(scope[strings.Index(scope, ":")+1:])
 	}
 	t.Print()
 }
