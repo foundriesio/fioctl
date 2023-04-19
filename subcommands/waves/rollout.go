68e14363ab65c70002c9c5f70cd430f256dcea84
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2023-04-19 18:56:04 +0300

    Cleanup: make waves rollout 'group' argument a named flag
    This is a prerequisite to make it optional and also add more selector flags.
    Otherwise, the command help message and overall look and feel may become confusing.
    Still, accept group name as a positional argument for backward compatibility.
    
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- subcommands/waves/rollout.go
+++ subcommands/waves/rollout.go
@@ -1,6 +1,8 @@
 package waves
 
 import (
+	"errors"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -10,8 +12,8 @@ import (
 )
 
 func init() {
-	cmd.AddCommand(&cobra.Command{
-		Use:   "rollout <wave> <group>",
+	rollout := &cobra.Command{
+		Use:   "rollout <wave> -g <group>",
 		Short: "Rollout a given wave to devices in a given device group",
 		Long: `Rollout a given wave to devices in a given device group.
 Upon rollout a wave becomes available as an update source for production devices in a specific
@@ -19,16 +21,35 @@ device group.  An rollout is not instant, but rather each device in a given grou
 this wave's targets at some point in time which is determined by many factors: most important being
 network conditions between a device and update servers, as well as a device update schedule.`,
 		Run:  doRolloutWave,
-		Args: cobra.ExactArgs(2),
-	})
+		Args: cobra.RangeArgs(1, 2),
+	}
+	rollout.Flags().StringP("group", "g", "", "A device group to roll out a wave to")
+	cmd.AddCommand(rollout)
 }
 
 func doRolloutWave(cmd *cobra.Command, args []string) {
 	factory := viper.GetString("factory")
 	wave := args[0]
-	group := args[1]
+	group := readGroup(cmd, args)
+	if len(group) == 0 {
+		subcommands.DieNotNil(errors.New("Required flag \"group\" not set\n" + cmd.UsageString()))
+	}
 	logrus.Debugf("Rolling out a wave %s for %s to %s", wave, factory, group)
 
 	options := client.WaveRolloutOptions{Group: group}
 	subcommands.DieNotNil(api.FactoryRolloutWave(factory, wave, options))
+}
+
+func readGroup(cmd *cobra.Command, args []string) string {
+	// Backward-compatible reader: new way - named flag, old way - positional flag
+	group, _ := cmd.Flags().GetString("group")
+	if len(args) > 1 {
+		if len(group) > 0 {
+			subcommands.DieNotNil(errors.New(
+				"Flag \"group\" cannot be both positional and named\n" + cmd.UsageString(),
+			))
+		}
+		group = args[1]
+	}
+	return group
 }
