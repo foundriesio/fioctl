d65a5b6f65181dacee1204daafa1791dff024434
Andy Doan <andy@foundries.io>
2023-08-01 16:11:03 -0500

    version: Add logic to self-update
    Signed-off-by: Andy Doan <andy@foundries.io>

--- subcommands/version/cmd.go
+++ subcommands/version/cmd.go
@@ -10,13 +10,16 @@ import (
 )
 
 var Commit string
+var updateTo string
 
 func NewCommand() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Show version information of this tool.",
 		Run:   doVersion,
 	}
+	cmd.Flags().StringVarP(&updateTo, "update-to", "", "", "Update to the given version of fioctl")
+	return cmd
 }
 
 func doVersion(cmd *cobra.Command, args []string) {
@@ -24,9 +27,29 @@ func doVersion(cmd *cobra.Command, args []string) {
 	updateFinder := newUpdateFinder(cmd)
 	update, err := updateFinder.FindLatest()
 	DieNotNil(err)
+
+	if len(updateTo) != 0 {
+		if updateTo != update.Version.String() {
+			DieNotNil(fmt.Errorf("invalid version %s != %s", updateTo, update.Version))
+		}
+		if err := update.Do(); err != nil {
+			if os.IsPermission(err) {
+				fmt.Println("There was a permission error while updating fioctl. Please run this again with 'sudo'")
+			} else {
+				DieNotNil(err)
+			}
+		}
+		return
+	}
+
 	if update != nil {
 		fmt.Println("Update available:", update.Version)
 		fmt.Println("\t", update.Uri)
+		exe, err := os.Executable()
+		DieNotNil(err)
+		fmt.Println()
+		fmt.Println("You can update by running:")
+		fmt.Println("\t", exe, "version", "--update-to", update.Version)
 	} else {
 		fmt.Println("This is the latest version of the tool.")
 	}
