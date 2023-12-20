a3b5bd3fed849897035e8f5aa300af54a31a48c1
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2023-12-20 17:11:03 +0200

    Feature: ability to manually re-sign wave targets on TUF root updates
    Sometimes, a user might need to re-sign active wave targets manually.
    The following use cases apply:
    
    - A user removed some offline TUF targets key, so they need to re-sign a part of waves using a different key.
    - A user started a new wave during an active TUF root updates transaction, so they need add a new signature for that wave.
    
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- subcommands/keys/tuf_updates_sign_prod_targets.go
+++ subcommands/keys/tuf_updates_sign_prod_targets.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	tuf "github.com/theupdateframework/notary/tuf/data"
 
 	"github.com/foundriesio/fioctl/subcommands"
 )
@@ -32,6 +33,7 @@ There are 3 use cases when this command comes handy:
 	_ = signCmd.MarkFlagFilename("keys")
 	_ = signCmd.MarkFlagRequired("keys")
 	signCmd.Flags().StringP("tags", "", "", "A comma-separated list of tags to sign; default: all tags.")
+	signCmd.Flags().StringP("waves", "", "", "A comma-separated list of waves to sign; default: all active waves.")
 	tufUpdatesCmd.AddCommand(signCmd)
 }
 
@@ -40,9 +42,13 @@ func doTufUpdatesSignProdTargets(cmd *cobra.Command, args []string) {
 	txid, _ := cmd.Flags().GetString("txid")
 	keysFile, _ := cmd.Flags().GetString("keys")
 	tagsStr, _ := cmd.Flags().GetString("tags")
-	var tags []string
+	wavesStr, _ := cmd.Flags().GetString("waves")
+	var tags, waveNames []string
 	if tagsStr != "" {
 		tags = strings.Split(tagsStr, ",")
+	}
+	if wavesStr != "" {
+		waveNames = strings.Split(wavesStr, ",")
 	}
 
 	creds, err := GetOfflineCreds(keysFile)
@@ -65,12 +71,25 @@ For example, add a new offline TUF targets key, before signing production target
 		subcommands.SliceRemove(newCiRoot.Signed.Roles["targets"].KeyIDs, onlineTargetsId))
 	subcommands.DieNotNil(err)
 
+	var newTargetsProdSigs, newTargetsWaveSigs map[string][]tuf.Signature
+
 	fmt.Println("= Signing prod targets")
-	targetsMap, err := api.ProdTargetsList(factory, true, tags...)
-	subcommands.DieNotNil(err, "Failed to fetch production targets:")
-	newTargetsSigs, err := signProdTargets(signer, targetsMap)
-	subcommands.DieNotNil(err)
+	// If both wave names and tags specified, or none specified - re-sign both prod and wave targets.
+	// If only wave names or only tags specified - re-sign only what was specified (either wave names or tags).
+	if len(tags) > 0 || len(waveNames) == 0 {
+		targetsProdMap, err := api.ProdTargetsList(factory, true, tags...)
+		subcommands.DieNotNil(err, "Failed to fetch production targets:")
+		newTargetsProdSigs, err = signProdTargets(signer, targetsProdMap)
+		subcommands.DieNotNil(err)
+	}
+	if len(waveNames) > 0 || len(tags) == 0 {
+		targetsWaveMap, err := api.WaveTargetsList(factory, true, waveNames...)
+		subcommands.DieNotNil(err, "Failed to fetch production wave targets:")
+		newTargetsWaveSigs, err = signProdTargets(signer, targetsWaveMap)
+		subcommands.DieNotNil(err)
+	}
 
 	fmt.Println("= Uploading new signatures")
-	subcommands.DieNotNil(api.TufRootUpdatesPut(factory, txid, newCiRoot, newProdRoot, newTargetsSigs, nil))
+	subcommands.DieNotNil(api.TufRootUpdatesPut(
+		factory, txid, newCiRoot, newProdRoot, newTargetsProdSigs, newTargetsWaveSigs))
 }
