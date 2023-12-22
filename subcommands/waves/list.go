70098242411ad155cc468d3b86c78e5bcef14bbb
Volodymyr Khoroz <volodymyr.khoroz@foundries.io>
2023-12-22 16:32:11 +0200

    Feature: support wave filtering by status
    Signed-off-by: Volodymyr Khoroz <volodymyr.khoroz@foundries.io>

--- subcommands/waves/list.go
+++ subcommands/waves/list.go
@@ -18,17 +18,17 @@ func init() {
 	cmd.AddCommand(listCmd)
 	listCmd.Flags().Uint64P("limit", "n", 20, "Limit the number of results displayed.")
 	listCmd.Flags().Uint64P("page", "p", 1, "Page of waves to display when pagination is needed")
-	listCmd.Flags().BoolP("only-active", "", false, "Only show currently active waves")
+	listCmd.Flags().StringP("status", "S", "", "Only show waves with a given status; one of (active, complete, canceled)")
 }
 
 func doListWaves(cmd *cobra.Command, args []string) {
 	factory := viper.GetString("factory")
 	limit, _ := cmd.Flags().GetUint64("limit")
 	showPage, _ := cmd.Flags().GetUint64("page")
-	onlyActive, _ := cmd.Flags().GetBool("only-active")
+	status, _ := cmd.Flags().GetString("status")
 	logrus.Debugf("Showing a list of waves for %s", factory)
 
-	lst, err := api.FactoryListWaves(factory, limit, showPage, onlyActive)
+	lst, err := api.FactoryListWaves(factory, limit, showPage, status)
 	subcommands.DieNotNil(err)
 
 	t := tabby.New()
