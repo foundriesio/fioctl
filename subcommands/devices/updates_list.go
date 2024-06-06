4a734d9aade5c5ba7cd9ef335edf1f425b835bf9
Andy Doan <andy@foundries.io>
2024-06-06 13:56:41 -0500

    device: Remove deprecated `updates_list`
    We deprecated this in 2021. Should be safe now.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- subcommands/devices/updates_list.go
+++ subcommands/devices/updates_list.go
@@ -10,18 +10,6 @@ import (
 	"github.com/foundriesio/fioctl/subcommands"
 )
 
-func init() {
-	listUpdatesCmd := &cobra.Command{
-		Use:    "list <device>",
-		Short:  "[DEPRECATED] Please use: fioctl devices updates <update-id>",
-		Hidden: true,
-		Run:    doListUpdates,
-		Args:   cobra.ExactArgs(1),
-	}
-	updatesCmd.AddCommand(listUpdatesCmd)
-	listUpdatesCmd.Flags().IntVarP(&listLimit, "limit", "n", 0, "Limit the number of results displayed.")
-}
-
 func doListUpdates(cmd *cobra.Command, args []string) {
 	factory := viper.GetString("factory")
 	logrus.Debug("Showing device updates")
